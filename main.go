27ea813ee7f2833a0b7b90185d5998daae72a021
Santiago Firpo <s.firpo@edu.pucrs.br>
2024-06-29 01:08:40 -0300

    WIP: merge sort with parallel merging

--- main.go
+++ main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"fmt"
 	"parallel-merge-sort/regularMergeSort"
+	"runtime"
 )
 
 func main() {
+	runtime.GOMAXPROCS(1)
 	array := []int{9, 8, 7, 6, 5, 4, 3, 2, 1}
-	regularMergeSort.MergeSort(array)
+	outputArray := make([]int, len(array))
+	syncChannel := make(chan struct{})
+	regularMergeSort.ParallelMergeSort(array, outputArray, syncChannel)
+	<-syncChannel
 	fmt.Printf("%v", array)
 }
--- regularMergeSort/regularMergeSort.go
+++ regularMergeSort/regularMergeSort.go
@@ -1,38 +1,74 @@
 package regularMergeSort
 
-func MergeSort(array []int) {
-	mergeSortWithCopy(array, 0, len(array)-1)
-}
-
-func mergeSortWithCopy(array []int, start int, end int) {
-	if start < end {
-		middle := (start + end) / 2
-		mergeSortWithCopy(array, start, middle)
-		mergeSortWithCopy(array, middle+1, end)
-		mergeWithCopy(array, start, middle, end)
+/**
+ * A: Input array
+ * B: Output array
+ * lo: lower bound
+ * hi: upper bound
+ * off: offset
+ */
+func ParallelMergeSort(inputArray []int, outputArray []int, syncChannel chan struct{}) {
+	length := uint(len(inputArray))
+	if length == 1 {
+		outputArray[0] = inputArray[0]
+	} else {
+		T := make([]int, length)
+		mid := length / 2
+		otherMid := mid - 1
+		mySyncChannel := make(chan struct{})
+		go ParallelMergeSort(inputArray[0:mid+1], T, mySyncChannel)
+		go ParallelMergeSort(inputArray[mid+1:length], T[otherMid+1:length], mySyncChannel)
+		<-mySyncChannel
+		<-mySyncChannel
+		parallelMerge(T[1:otherMid+1], T[otherMid+1:length], outputArray, syncChannel)
 	}
 }
 
-func mergeWithCopy(array []int, start, mid int, end int) {
-	leftSlice := make([]int, end-start+1)
-	rightSlice := make([]int, end-start+1)
-	copy(leftSlice, array[start:mid+1])
-	copy(rightSlice, array[mid+1:end+1])
-	i := 0
-	j := 0
-	for k := start; k <= end; k++ {
-		if i == len(leftSlice) {
-			array[k] = rightSlice[j]
-			j++
-		} else if j == len(rightSlice) {
-			array[k] = leftSlice[i]
-			i++
-		} else if leftSlice[i] < rightSlice[j] {
-			array[k] = leftSlice[i]
-			i++
+func parallelMerge(left []int, right []int, outputArray []int, syncChannel chan struct{}) {
+
+	leftLength := uint(len(left))
+	rightLength := uint(len(right))
+	outputLength := uint(len(outputArray))
+
+	if leftLength < rightLength {
+		//swap A and B  // ensure that A is the larger array: i, j still belong to A; k, ℓ to B
+
+		//swap leftLength and rightLength
+	}
+
+	if leftLength <= 0 {
+		return // base case, nothing to merge
+	}
+
+	leftMiddle := leftLength / 2
+	s := binarySearch(left[leftMiddle], right)
+	if s == -1 {
+		return
+	}
+	t := (leftMiddle) + (uint(s))
+	outputArray[t] = left[leftMiddle]
+
+	mySyncChannel := make(chan struct{})
+	go parallelMerge(left[0:leftMiddle+1], right[0:s+1], outputArray[0:t+1], mySyncChannel)
+	go parallelMerge(left[leftMiddle+1:leftLength], right[s:rightLength], outputArray[t+1:outputLength], mySyncChannel)
+	<-mySyncChannel
+	<-mySyncChannel
+	syncChannel <- struct{}{}
+}
+
+func binarySearch(key int, array []int) int {
+	low := 0
+	high := len(array) - 1
+
+	for low <= high {
+		mid := (low + high) / 2
+		if array[mid] < key {
+			low = mid + 1
+		} else if array[mid] > key {
+			high = mid - 1
 		} else {
-			array[k] = rightSlice[j]
-			j++
+			return mid
 		}
 	}
+	return -1
 }
