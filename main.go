f448d5e9150c97afe78c01b9c6f160be9e0b6b4d
Santiago Firpo <s.firpo@edu.pucrs.br>
2024-06-30 01:59:04 -0300

    WIP: measuring sorting time

--- main.go
+++ main.go
@@ -3,12 +3,18 @@ package main
 import (
 	"fmt"
 	"parallel-merge-sort/mergeSort/mergeSortWithParallelMergeAndSplit"
-	"parallel-merge-sort/mergeSort/mergeSortWithParallelSplit"
 )
 
 func main() {
-	arrayToSort := []int{903943, 38, 27, 43, 3, 9, 82, 10, 1, 2, 3, 4, 5, 6, 7, 9, 10}
-	fmt.Println(arrayToSort, "original array")
-	fmt.Println(mergeSortWithParallelMergeAndSplit.ParallelMergeSort(arrayToSort), "(Parallel Merge Sort with Parallel Merge and Split, sorted array)")
-	fmt.Println(mergeSortWithParallelSplit.ParallelMergeSort(arrayToSort), "(Parallel Merge Sort with Parallel Split, sorted array)")
+	arrayToSort := generateDecreasingArray(1000000)
+	mergeSortWithParallelMergeAndSplit.ParallelMergeSort(arrayToSort)
+	fmt.Println("Finished sorting")
+}
+
+func generateDecreasingArray(size int) []int {
+	array := make([]int, size)
+	for i := 0; i < size; i++ {
+		array[i] = size - i
+	}
+	return array
 }
