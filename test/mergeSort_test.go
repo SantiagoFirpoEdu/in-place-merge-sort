48deaf3d728a2f02e72fada41acd201c347b4049
Santiago Firpo <s.firpo@edu.pucrs.br>
2024-07-01 05:08:22 -0300

    Removed unused benchmarks

--- test/mergeSort_test.go
+++ test/mergeSort_test.go
@@ -9,31 +9,6 @@ import (
 	"testing"
 )
 
-// Make some benchmarks for comparison (sequential and parallel)
-func BenchmarkMergeSortWithParallelMergeAndSplit(b *testing.B) {
-	array := []int{38, 27, 43, 3, 9, 82, 10}
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		mergeSortParallelInPlace.MergeSort(array)
-	}
-}
-
-func BenchmarkMergeSortWithParallelSplit(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		b.ResetTimer()
-		array := []int{38, 27, 43, 3, 9, 82, 10}
-		mergeSortSequential.MergeSort(array)
-	}
-}
-
-func BenchmarkSequentialMergeSort(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		b.ResetTimer()
-		array := []int{38, 27, 43, 3, 9, 82, 10}
-		mergeSortSequential.MergeSort(array)
-	}
-}
-
 func TestSequentialMergeSort(t *testing.T) {
 	size1 := 10
 	arrayToSort1 := utils.GenerateDecreasingArray(size1)
