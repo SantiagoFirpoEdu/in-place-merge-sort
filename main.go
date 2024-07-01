29d9b833b0070a6e1435399e48015fe35e813451
Santiago Firpo <s.firpo@edu.pucrs.br>
2024-07-01 03:22:32 -0300

    Improved iteration time

--- main.go
+++ main.go
@@ -2,28 +2,35 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"parallel-merge-sort/mergeSort/mergeSortSequential"
 	"parallel-merge-sort/mergeSort/mergeSortWithParallelMergeAndSplit"
 	"parallel-merge-sort/mergeSort/mergeSortWithParallelSplit"
 	"parallel-merge-sort/utils"
-	"runtime"
 	"slices"
-	"sort"
 	"time"
 )
 
 func main() {
-	n := 6
-	runtime.GOMAXPROCS(n)
-	fmt.Println("Number of CPUs used:", n)
-	for _, size := range []int{100_000_000, 200_000_000, 400_000_000, 800_000_000} {
+	for _, size := range []int{100_000_000, 200_000_000, 300_000_000, 400_000_000} {
 		fmt.Println("Size:", size)
-		testMergeSorts(size)
+		if os.Args[0] == "sequential" {
+			fmt.Println("Sequential Merge Sort")
+			testSequentialMergeSorts(size)
+		} else if os.Args[0] == "parallel" {
+			fmt.Println("Parallel Merge Sort")
+			testParallelMergeSorts(size)
+		} else {
+			fmt.Println("Parallel Merge Sort")
+			testParallelMergeSorts(size)
+			fmt.Println("Sequential Merge Sort")
+			testSequentialMergeSorts(size)
+		}
 		fmt.Println()
 	}
 }
 
-func testMergeSorts(size int) {
+func testSequentialMergeSorts(size int) {
 	arrayToSort := utils.GenerateDecreasingArray(size)
 	start := time.Now()
 	mergeSortSequential.MergeSort(arrayToSort)
@@ -38,14 +45,11 @@ func testMergeSorts(size int) {
 	})
 	sortStableFuncDuration := time.Since(startSortStableFunc).Seconds()
 	fmt.Println("[SortStableFunc (Standard Library)] Finished in", sortStableFuncDuration, "s")
+}
 
-	arrayToSortCopy2 := make([]int, len(arrayToSort))
-	copy(arrayToSortCopy2, arrayToSort)
-	startSortStable := time.Now()
-	sort.Stable(sort.IntSlice(arrayToSortCopy2))
-	sortStableDuration := time.Since(startSortStable).Seconds()
-	fmt.Println("[sort.Stable (Standard Library)] Finished in", sortStableDuration, "s")
-
+func testParallelMergeSorts(size int) {
+	fmt.Println("Core Count:", os.Getenv("GOMAXPROCS"))
+	arrayToSort := utils.GenerateDecreasingArray(size)
 	startParallelSplit := time.Now()
 	mergeSortWithParallelSplit.MergeSort(arrayToSort)
 	parallelSplitDuration := time.Since(startParallelSplit).Seconds()
